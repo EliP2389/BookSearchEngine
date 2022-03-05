3e6a177830f946751a839b8c9fb4844725136ed1
Eli Pagan <mbk8689@gmail.com>
2022-03-05 16:37:42 -0500

    added Query and Mutations to typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -10,10 +10,9 @@ type User {
 }
 
 type Book {
-    _id: ID
+    bookId: String
     authors: String
     description: String
-    bookId: String
     image: String
     link: String
     title: String
@@ -22,4 +21,18 @@ type Book {
 type Auth {
     token: ID!
     user: User
-}`
+}
+
+type Query {
+    me: User
+}
+
+type Mutation {
+    login:(email: String!, password: String!): Auth
+    addUser:(username: String!, email: String!, password: String!): Auth
+    saveBook:([authors: String!], description: String!, title: String!, bookId: String!, image: String!, link: String!): User
+    removeBook:(bookId: String!): User
+}
+`;
+
+module.exports = typeDefs;
