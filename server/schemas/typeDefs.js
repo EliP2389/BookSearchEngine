3f48b0babc514b6ffef2ddf4286d4baf52a4ea72
Eli Pagan <mbk8689@gmail.com>
2022-03-05 16:10:45 -0500

    added Book to typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -7,4 +7,14 @@ type User {
     email: String
     bookCount: Int
     savedBooks: [Book]
+}
+
+type Book {
+    _id: ID
+    authors: String
+    description: String
+    bookId: String
+    image: String
+    link: String
+    title: String
 }`
