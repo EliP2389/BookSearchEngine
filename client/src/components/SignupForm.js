64f7d750ea592e59dd9bad15baef24334d424603
Eli Pagan <mbk8689@gmail.com>
2022-03-07 00:05:45 -0500

    added add_usee to signupform

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -4,6 +4,9 @@ import { Form, Button, Alert } from 'react-bootstrap';
 import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
 
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
+
 const SignupForm = () => {
   // set initial form state
   const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
@@ -11,6 +14,8 @@ const SignupForm = () => {
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
+
+  const [adduser, { error }] = useMutation(ADD_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -28,15 +33,17 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      const {data} = await adduser({
+        variables: { ...userFormData }
+      })
 
       if (!response.ok) {
         throw new Error('something went wrong!');
       }
 
-      const { token, user } = await response.json();
+      // const { token, user } = await response.json();
       console.log(user);
-      Auth.login(token);
+      Auth.login(data.adduser.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
